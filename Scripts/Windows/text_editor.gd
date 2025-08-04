acdac8e7d8305e627764e72b807f4710e844c699
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-08-04 16:33:37 +0530

    Allow setting the directory path outside of instantiation
    Requirement for the tutorial

--- Scripts/Windows/text_editor.gd
+++ Scripts/Windows/text_editor.gd
@@ -2,6 +2,7 @@ extends VirtualWindow
 
 @export var padding_size:Vector2 = Vector2(10,10)
 @export_multiline var text:String
+@export var directory_path_text:String
 
 @export var main_text:RichTextLabel
 @export var directory_label:RichTextLabel
@@ -10,3 +11,4 @@ func _ready():
 	#$Contents/Margin.size = $Contents.size - padding_size # Reduces size by padding
 	#$Contents/Margin.position.x = padding_size.x/2 # Re aligns text according to spacer
 	main_text.text = text # Sets the main text content
+	if directory_path_text: directory_label.text = directory_path_text
