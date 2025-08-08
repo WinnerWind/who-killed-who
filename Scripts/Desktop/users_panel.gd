a95d3e416d13db2be7c2501456356d753b437cc9
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-08-08 12:16:23 +0530

    Add a variable slice to users_panel
    This lets us set users more clearly

--- Scripts/Desktop/users_panel.gd
+++ Scripts/Desktop/users_panel.gd
@@ -3,11 +3,12 @@ class_name UsersPanel
 
 @onready var desktop:Desktop = get_owner()
 
+@export var slice:int = 1 #Determines what index onwards to not remove from the panel.
 @export var users_row:PackedScene
 
 @export var sorter:BoxContainer
 func add_users():
-	for user_row in sorter.get_children().slice(1):
+	for user_row in sorter.get_children().slice(slice):
 		user_row.queue_free()
 		
 	for user in desktop.users:
